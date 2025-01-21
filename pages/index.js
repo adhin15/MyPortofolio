e8141e83450094c7d1d523777a3fa421c3eb9ac0
Adhin <adhin.alifarchan@ist.id>
2025-01-21 18:20:16 +0700

    update meta tags

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,18 @@ import Homepage from "./homepage/homepage";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  
   return (
     <>
       <Head>
         <title>Adhin Alifarchan - My Works</title>
         <meta name="description" content="Welcome to Adhin's page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="./assets/moflix.jpg" />
         <link rel="icon" href="/logo/a-logo.png" />
         <link rel="stylesheet" href="/style.scss" />
-        <link href="https://fonts.cdnfonts.com/css/montserrat" rel="stylesheet"/>
+        <link href="https://fonts.cdnfonts.com/css/montserrat" rel="stylesheet" />
       </Head>
-      <Homepage/>
+      <Homepage />
     </>
   );
 }
