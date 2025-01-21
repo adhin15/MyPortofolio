03f2847e471e71eb67079fdb4115263e7f1f7ea7
Adhin <adhin.alifarchan@ist.id>
2025-01-21 10:29:41 +0700

    update portfolio

--- components/section/Homepage_section/Portfolio.js
+++ components/section/Homepage_section/Portfolio.js
@@ -13,7 +13,7 @@ const Portfolio = () => {
           <div className="p-3 pl-0 max-w-[350px]">
             <div className="p-2 rounded-lg bg-[#2A2550] h-[300px] m-3 ml-0" id="website-card">
               <a href="https://ressambas.kalbar.polri.go.id" target="_blank" rel="noreferrer">
-                <img src="./assets/ressambas.png" />
+                <img src="./assets/ressambas.jpg" />
                 <p className="text-sm text-[#FF7700] text-center my-3">https://ressambas.kalbar.polri.go.id</p>
                 <div className="px-4 text-center">
                   <p className="block text-xs bg-[#FF7700] font-bold rounded-lg p-2 px-5 w-full m-auto max-w-[200px] cursor-pointer">
@@ -28,7 +28,7 @@ const Portfolio = () => {
           {/* -----------------card----------------- */}
           <div className="p-3 pl-0 max-w-[350px]">
             <div className="p-2 rounded-lg bg-[#2A2550] h-[300px] m-3 ml-0" id="website-card">
-              <img src="./assets/btn-properti.png" />
+              <img src="./assets/btn-properti.jpg" />
               <p className="text-sm text-[#FF7700] text-center my-3">https://www.btnproperti.co.id</p>
               <div className="px-4 text-center">
                 <a
@@ -51,7 +51,7 @@ const Portfolio = () => {
           {/* -----------------card----------------- */}
           <div className="p-2 rounded-lg bg-[#2A2550] max-w-[350px] h-[300px] m-3 ml-0" id="website-card">
             <a href="https://pokemon-dexter.vercel.app" target="_blank" rel="noreferrer">
-              <img src="./assets/pokemon-dexter.png" />
+              <img src="./assets/pokemon-dexter.jpg" />
               <p className="text-sm text-[#FF7700] text-center my-3">https://pokemon-dexter.vercel.app</p>
               <div className="px-4 text-center">
                 <p className="block text-xs bg-[#FF7700] font-bold rounded-lg p-2 px-5 w-full m-auto max-w-[200px] cursor-pointer">
@@ -63,8 +63,20 @@ const Portfolio = () => {
 
           <div className="p-2 rounded-lg bg-[#2A2550] max-w-[350px] h-[300px] m-3 ml-0" id="website-card">
             <a href="https://mo-flixx.vercel.app" target="_blank" rel="noreferrer">
-              <img src="./assets/moflix.png" />
+              <img src="./assets/moflix.jpg" />
               <p className="text-sm text-[#FF7700] text-center my-3">https://mo-flixx.vercel.app</p>
+              <div className="px-4 text-center">
+                <p className="block text-xs bg-[#FF7700] font-bold rounded-lg p-2 px-5 w-full m-auto max-w-[200px] cursor-pointer">
+                  Visit
+                </p>
+              </div>
+            </a>
+          </div>
+
+          <div className="p-2 rounded-lg bg-[#2A2550] max-w-[350px] h-[300px] m-3 ml-0" id="website-card">
+            <a href="https://urban-luxe.vercel.app/" target="_blank" rel="noreferrer">
+              <img src="./assets/urban-luxe.jpg" />
+              <p className="text-sm text-[#FF7700] text-center my-3">https://urban-luxe.vercel.app/</p>
               <div className="px-4 text-center">
                 <p className="block text-xs bg-[#FF7700] font-bold rounded-lg p-2 px-5 w-full m-auto max-w-[200px] cursor-pointer">
                   Visit
